46938a02eb467f3b44e31ebdaf6431ad82a31512
Dmitriy Filatov <filatov-d@mail.ru>
2022-09-15 19:01:36 +0300

    Example 28 completed

---
+++ seminar4/Example28/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Напишите программу, которая принимает на вход число N и
+выдает произведение чисел от 1 до N.
+*/
+
+int GetMulti(int number)
+{
+    int multi = 1;
+    if (number > 0)
+    {
+        // Перебор всех чисел от 1 до number
+        for (int i = 1; i <= number; i++)
+        {
+            multi = multi * i;
+        }
+        
+    }
+
+    else multi = 0;
+
+    return multi;
+}
+
+Console.Write("Imput N: ");
+int n = int.Parse(Console.ReadLine());
+
+int multiplication = GetMulti(n);
+Console.WriteLine(multiplication);
